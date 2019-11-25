78600936a9fe9ec32f516d41adeb0fd72e5166bb
Jermel Babb <developers@octane5.net>
2019-11-25 18:17:23 -0500

    add selectize support

--- django_admin_listfilter_dropdown/filters.py
+++ django_admin_listfilter_dropdown/filters.py
@@ -25,3 +25,23 @@ class RelatedDropdownFilter(RelatedFieldListFilter):
 
 class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
+
+
+class SelectizeSimpleDropdownFilter(SimpleListFilter):
+    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+
+
+class SelectizeDropdownFilter(AllValuesFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+
+
+class SelectizeChoiceDropdownFilter(ChoicesFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+
+
+class SelectizeRelatedDropdownFilter(RelatedFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+
+
+class SelectizeRelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
