4ae6cea38dea4f9f6ac86cfc378aac939aa06c84
Jermel Babb <developers@octane5.net>
2019-11-25 21:52:59 -0500

    use chosen jquery library by default.  No need for additional classes

--- django_admin_listfilter_dropdown/filters.py
+++ django_admin_listfilter_dropdown/filters.py
@@ -25,23 +25,3 @@ class RelatedDropdownFilter(RelatedFieldListFilter):
 
 class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
-
-
-class ChosenSimpleDropdownFilter(SimpleListFilter):
-    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
-
-
-class ChosenDropdownFilter(AllValuesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
-
-
-class ChosenChoiceDropdownFilter(ChoicesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
-
-
-class ChosenRelatedDropdownFilter(RelatedFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
-
-
-class ChosenRelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
