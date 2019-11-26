3133958ad5392f7d73d3413c58c3b54f4cd09df3
Jermel Babb <developers@octane5.net>
2019-11-25 19:20:44 -0500

    switch to chosen

--- django_admin_listfilter_dropdown/filters.py
+++ django_admin_listfilter_dropdown/filters.py
@@ -27,21 +27,21 @@ class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
     template = 'django_admin_listfilter_dropdown/dropdown_filter.html'
 
 
-class SelectizeSimpleDropdownFilter(SimpleListFilter):
-    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+class ChosenSimpleDropdownFilter(SimpleListFilter):
+    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
 
 
-class SelectizeDropdownFilter(AllValuesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+class ChosenDropdownFilter(AllValuesFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
 
 
-class SelectizeChoiceDropdownFilter(ChoicesFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+class ChosenChoiceDropdownFilter(ChoicesFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
 
 
-class SelectizeRelatedDropdownFilter(RelatedFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+class ChosenRelatedDropdownFilter(RelatedFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
 
 
-class SelectizeRelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
-    template = 'django_admin_listfilter_dropdown/selectize_dropdown_filter.html'
+class ChosenRelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
+    template = 'django_admin_listfilter_dropdown/chosen_dropdown_filter.html'
